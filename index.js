a22a27370e42fa824f10d514cb600a6aec5213de
shawon <shawonislam1109@gmail.com>
2024-09-02 22:36:27 +0600

    index server is change

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const app = express();
 const cron = require("node-cron");
+const app = express();
 
-// import
+// Import modules
 const setRouters = require("./Routes/mainRoutes");
 const setMiddleware = require("./middleware/mainMiddleware");
 const setErrorHandler = require("./errorHandler/errorHandler");
 const { createEachBorderMill } = require("./src/millCount/controller");
 
-// Usings middleware from middleware directory
+// Use middleware
 setMiddleware(app);
 
-// Usings route from route directory
+// Use routes
 setRouters(app);
 
-// import error handler
+// Error handling middleware
 setErrorHandler(app);
 
 // Cron job for Task A at 11:59 PM every day
 cron.schedule("59 23 * * *", () => {
   createEachBorderMill();
 });
-// cron.schedule("* * * * *", () => {
-//   createEachBorderMill();
-//   console.log("cron is executed");
-// });
 
+// Environment variables
 const port = process.env.PORT || 9190;
-
-const clientOptions = {
-  serverApi: { version: "1", strict: true, deprecationErrors: true },
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-// BD USER AND PASSWORD
 const user = process.env.USERNAME_BD;
 const password = process.env.DB_PASSWORD;
+
 async function run() {
   try {
-    // Correct the connection string format
-    // const uri = `mongodb+srv://mill_manage_ment:${password}@cluster0.5rnuhbi.mongodb.net/millManagement?ssl=true&replicaSet=atlas-xxx-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    const uri = `mongodb+srv://mill_manage_ment:${password}@cluster0.5rnuhbi.mongodb.net/millManagement?ssl=true&retryWrites=true&w=majority`;
-    // Create a Mongoose client with the MongoClientOptions object
-    await mongoose.connect(uri, clientOptions);
+    const uri = `mongodb+srv://mill_manage_ment:Mzo9xp69yF8axaQr@cluster0.5rnuhbi.mongodb.net/millManagement?ssl=true&retryWrites=true&w=majority`;
 
-    // Start the Express.js server
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
